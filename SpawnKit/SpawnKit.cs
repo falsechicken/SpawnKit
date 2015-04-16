ae469267639ae14d6e5449ec8474dbd16d056c1f
False_Chicken <jmdevsupport@gmail.com>
2015-04-15 21:33:24 -0400

    Added proper license headers to source files.

--- SpawnKit/SpawnKit.cs
+++ SpawnKit/SpawnKit.cs
@@ -1,10 +1,22 @@
-﻿/**
- * AnnounceBot plugin for the Rocket Unturned Server Wrapper.
+﻿/*****
+ * SpawnKit - Rocket Unturned Server Wrapper plugin providing spawn kits, randomized kits, and more.
  * 
- * Licensed under the GPLv2.
+ * Copyright (C) 2015 False_Chicken
+ * Contact: jmdevsupport@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
- * False_Chicken
- * */
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, Get it here: https://www.gnu.org/licenses/gpl-2.0.html
+ *****/
 
 using System;
 using System.Collections.Generic;
--- SpawnKit/SpawnKitConfiguration.cs
+++ SpawnKit/SpawnKitConfiguration.cs
@@ -1,6 +1,23 @@
-﻿/*
-
- */
+﻿/*****
+ * SpawnKitConfiguration - Configuration object for SpawnKit
+ * 
+ * Copyright (C) 2015 False_Chicken
+ * Contact: jmdevsupport@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, Get it here: https://www.gnu.org/licenses/gpl-2.0.html
+ *****/
+ 
 using System;
 using Rocket.RocketAPI;
 using System.Collections.Generic;
@@ -9,9 +26,6 @@ using fc.itemdb;
 
 namespace fc.spawnkit
 {
-	/// <summary>
-	/// Description of SpawnKitConfiguration.
-	/// </summary>
 	public class SpawnKitConfiguration : RocketConfiguration
 	{
 		[XmlArrayItem(ElementName = "Kit")]
